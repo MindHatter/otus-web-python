c3383140f7daa6c03a04a643c463a2d63c46c8fd
Aleksandr Menshikov <menshikov.abz@gmail.com>
2021-04-11 10:12:12 +0300

    EDU. Add course views & templates

--- 04_edu/courses/views.py
+++ 04_edu/courses/views.py
@@ -1,3 +1,14 @@
+from django.views.generic import DetailView, ListView
 from django.shortcuts import render
 
+from .models import Course
+
 # Create your views here.
+class CourseListView(ListView):
+    model = Course
+    page_name = 'курсы'
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    page_name = 'публикация'
--- 04_edu/edu/settings.py
+++ 04_edu/edu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zz4=@=aj6hpk&!2_6w&z(+%-&ts8g_9t1)@x%4ypl(q2-7&w2e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'edu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
